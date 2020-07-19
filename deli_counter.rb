cc064041d67bf899e3bad22b7db0fcc85031aa94
Dayami Velazquez <dayami.velazquez@gmail.com>
2020-07-19 11:26:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,12 +7,9 @@ def line(deli)
     puts "The line is currently empty."
   else
   current_line = "The line is currently:"
-  
   deli.each_with_index(1) do |person, i|
    current_line << " #{i}, #{person}"
    end
    puts current_line
-
 end
-
 end
